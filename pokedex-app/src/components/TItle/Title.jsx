8b466d4ef1f6c8d27cea421beb711feaf71936eb
alexandru-alin.vezeteu <Alexandru-Alin.Vezeteu@endava.com>
2022-03-16 15:58:10 +0200

    [feature] - clicking on title takes you home

--- pokedex-app/src/components/TItle/Title.jsx
+++ pokedex-app/src/components/TItle/Title.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Title.css';
 
+import { Link } from 'react-router-dom'
+
 function Title({ children }) {
-  return <h1 className="title-text">{children}</h1>;
+  return (
+    <Link to = '/home' style = {{"text-decoration": "none"}}>
+      <h1 className="title-text">{children}</h1>
+    </Link>
+  )
 }
 
 export default Title;
